33293b53419739688e0ffc2d90faee6ff63c6da0
pogorelov-ss <pogorelov.ss@gmail.com>
2013-10-14 23:34:40 +0300

    работает выбор person+groups

--- adressbook.js
+++ adressbook.js
@@ -10,6 +10,12 @@ var ADRBOOK = ADRBOOK || {
     groupsDom: d3.select("#groups")
         .append("select")
         .attr("size", 5),
+    personsAddDom: d3.select("#persons").append("button")
+        .attr("value", "")
+        .text("Add")
+        .on("click", function () {
+            ADRBOOK.addPerson()
+        }),
     personsDom: d3.select("#persons")
         .append("select")
         .attr("size", 10),
@@ -17,9 +23,10 @@ var ADRBOOK = ADRBOOK || {
     searchDom: d3.select("#search")
         .append("input")
         .attr("type", "text")
-        .on("change", function () {
+        .on("keypress", function () {
             ADRBOOK.output.persons(ADRBOOK.findPersons("fio", this.value))
         }),
+    editDom: d3.select("#edit"),
     store: [],//массив в который заружается адресная книга
     /**
      * поиск вхождения строки в свойство, возвращает масив объектов в которых было совпадение
@@ -33,6 +40,7 @@ var ADRBOOK = ADRBOOK || {
         for (var i = 0; i < this.store.length; i++) {
             if (~this.store[i][propName].indexOf(what)) {
                 persons.push(this.store[i]);
+                persons[persons.length - 1].pid = i;//индекс в исходном массиве
             }
         }
         return persons;
@@ -50,18 +58,48 @@ var ADRBOOK = ADRBOOK || {
         return Object.keys(groups);
     },
     addPerson:function(){
+        var storeTMP = {};
+        for (prop in ADRBOOK.store[1]) {
+            storeTMP[prop] = prop;
+        }
+        storeTMP.pid = ADRBOOK.store.length;
+        ADRBOOK.output.new(storeTMP);
         alert('add')
     },
+    new: function (contact) {
+        ADRBOOK.store.push(contact);
+        ADRBOOK.output.init();
+    },
     delPerson:function(personId){
+        var storeTMP = [];
+        for (var i = 0; i < this.store.length; i++) {
+            if (i != personId) {
+                storeTMP.push(this.store[i]);
+            }
+        }
+        this.store = storeTMP;
+        ADRBOOK.output.init();
         alert('del')
     },
-    replacePerson:function(person){
+    replacePerson: function (contact) {
+        for (var i = 0; i < this.store.length; i++) {
+            if (i == contact.pid) {
+                this.store[i] = contact;
+            }
+        }
+        ADRBOOK.output.init();
         alert('edit')
     }
 
 };
 
 ADRBOOK.output = {
+    init: function () {
+        ADRBOOK.output.persons(ADRBOOK.findPersons("fio", ""));
+        ADRBOOK.output.groups(ADRBOOK.getGroups(ADRBOOK.store));
+        ADRBOOK.contactDom.selectAll("p").remove();
+        ADRBOOK.editDom.selectAll("*[value]").remove();
+    },
     persons:function (personsArray){
         ADRBOOK.personsDom.selectAll("option").remove();
         ADRBOOK.personsDom
@@ -98,11 +136,86 @@ ADRBOOK.output = {
     },
     contact:function (contact){
         ADRBOOK.contactDom.selectAll("p").remove();
-        for (var prop in contact) {
-            ADRBOOK.contactDom.append("p").text(prop + ": " + contact[prop]);
-        }
-
-
+        ADRBOOK.contactDom.selectAll("p")
+            .data(Object.keys(contact))
+            .enter().append("p")
+            .attr("value", function (d) {
+                return d
+            })
+            .text(function (d) {
+                return d + ":" + contact[d]
+            });
+        ADRBOOK.contactDom.append("p")
+            .attr("id", "command")
+            .append("a")
+            .text(" Edit ")
+            .on("click", function () {
+                ADRBOOK.output.edit(contact)
+            });
+        ADRBOOK.contactDom.select("#command")
+            .append("a")
+            .text(" Remove ")
+            .on("click", function () {
+                ADRBOOK.delPerson(contact.pid)
+            });
+    },
+    edit: function (contact) {
+        ADRBOOK.editDom.selectAll("*[value]").remove();
+        ADRBOOK.editDom.selectAll("input")
+            .data(Object.keys(contact))
+            .enter().append("input")
+            .attr("type", "text")
+            .attr("value", function (d) {
+                return contact[d]
+            })
+            .attr("id", function (d) {
+                return d
+            })
+            .on("keypress", function (d) {
+                contact[d] = this.value
+            });
+        ADRBOOK.editDom.select("input[id='pid']").attr("readonly", "");
+        ADRBOOK.editDom.append("button")
+            .attr("value", "")
+            .text("Save")
+            .on("click", function () {
+                ADRBOOK.replacePerson(contact)
+            })
+        ADRBOOK.editDom.append("button")
+            .attr("value", "")
+            .text("Cancel")
+            .on("click", function () {
+                ADRBOOK.output.init()
+            })
+    },
+    new: function (contact) {
+        ADRBOOK.editDom.selectAll("*[value]").remove();
+        ADRBOOK.editDom.selectAll("input")
+            .data(Object.keys(contact))
+            .enter().append("input")
+            .attr("type", "text")
+            .attr("value", function (d) {
+                return contact[d]
+            })
+            .attr("id", function (d) {
+                return d
+            })
+            .on("keypress", function (d) {
+                contact[d] = this.value
+            });
+        ADRBOOK.editDom.select("input[id='pid']").attr("readonly", "");
+        ADRBOOK.editDom.append("button")
+            .attr("value", "")
+            .text("Save")
+            .on("click", function () {
+                ADRBOOK.new(contact)
+            })
+        ADRBOOK.editDom.append("button")
+            .attr("value", "")
+            .text("Cancel")
+            .on("click", function () {
+                ADRBOOK.output.init()
+            })
     }
 
 };
@@ -112,32 +225,27 @@ ADRBOOK.output = {
  * отладочная часть
  */
 ADRBOOK.store.push({
-    pid:"1",
     fio: 'Иван Иванович',
     email: 'grey@in.ua',
     mobile: '5755',
     group: 'работа'
 }, {
-    pid:"2",
     fio: 'Алексей Петрович',
     email: 'grey@in.ua',
     mobile: '555',
     group: 'работа',
     заметка: 'да ну его "№ '
 },{
-    pid:"3",
     fio: 'Не помню кто это',
     email: 'grey@in.ua',
     mobile: '555',
     group: 'ХЗ'
 },{
-    pid:"4",
     fio: 'Mike Gray4',
     email: 'grey@in.ua',
     mobile: '555',
     group:'b'
 },{
-    pid:"5",
     fio: 'Mike Gray5',
     email: 'grey@in.ua',
     mobile: '555',
@@ -145,8 +253,6 @@ ADRBOOK.store.push({
 });
 
 //ADRBOOK.searchDom.append("option").text("div");
-ADRBOOK.output.persons(ADRBOOK.store);
-ADRBOOK.output.groups(ADRBOOK.getGroups(ADRBOOK.store));
-
+ADRBOOK.output.init();
 
 //alert(ADRBOOK.findPersons('mobile', '5')[0].fio);
